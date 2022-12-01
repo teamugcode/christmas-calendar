02dbf10923f810feefc5a0b877cf326ad7d353fd
Teemu Virenius <16792417+temesky@users.noreply.github.com>
2022-12-01 20:16:37 +0200

    Add Calendar to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Calendar from "./components/Calendar";
 import Header from "./components/Header";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Calendar />
 		</div>
 	);
 }
