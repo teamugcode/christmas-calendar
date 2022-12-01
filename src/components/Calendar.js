abb47effb80aae8c78f000d3571d9f65c895f610
Teemu Virenius <16792417+temesky@users.noreply.github.com>
2022-12-01 20:55:45 +0200

    Add open door click functionality

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,8 +5,27 @@ import Door from "./Door";
 const Calendar = () => {
 	const [doors, setDoors] = useState(randomizedDoors);
 
-	const openDoor = (doorNum) => {
-		console.log("openDoor", doorNum);
+	const openDoor = (clickedDoorNum) => {
+		// Find clicked door index from doors array
+		const currentDoorIndex = doors.findIndex(
+			(door) => door.num === clickedDoorNum
+		);
+
+		/*
+		 * Update clicked door isOpen value to true in doors array
+		 * - Create new array
+		 * - Add doors before current index with ...doors.slice(0, currentDoorIndex)
+		 * - Add current door with open status with { ...doors[currentDoorIndex], isOpen: true },
+		 * - Add doors after current index with ...doors.slice(currentDoorIndex + 1)
+		 */
+		const updatedDoors = [
+			...doors.slice(0, currentDoorIndex),
+			{ ...doors[currentDoorIndex], isOpen: true },
+			...doors.slice(currentDoorIndex + 1),
+		];
+
+		// Set updated doors array to state
+		setDoors(updatedDoors);
 	};
 
 	return (
